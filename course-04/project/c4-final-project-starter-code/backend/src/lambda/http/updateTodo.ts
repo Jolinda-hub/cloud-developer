58ab4ce7d4ad758366f77fb2992977ac8912645a
Jolinda-hub <60364539+Jolinda-hub@users.noreply.github.com>
2022-11-16 13:47:44 +0200

    Backend Update

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -3,20 +3,31 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
-
-//import { updateTodo } from '../../businessLogic/todos'
+import { updatedTodos } from '../../businessLogic/todos'
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 //import { getUserId } from '../utils'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    //const todoId = event.pathParameters.todoId
+    const todoId = event.pathParameters.todoId
     const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
-    console.log(updatedTodo)
     // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
+    try {
+      await updatedTodos(todoId, updatedTodo)
+  
+      return {
+        statusCode: 201,
+        body: ''
+      }
+    } catch (error) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          error
+        })
+      }
+    }
 
-
-    return undefined
   }
 )
 
--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 't4ccymvc3g'
+const apiId = 'bl0jkv313k'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
